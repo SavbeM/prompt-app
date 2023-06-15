d6daac5745bf85cac34276db647c24da9b117013
savbeM <mark.selihov2458@gmail.com>
2023-06-15 18:28:14 +0300

    Home page

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Home = () => {
+    return (
+        <section className="w-full flex flex-center flex-col">
+            <h1>
+                Discover & Share
+            </h1>
+        </section>
+    )
+}
+Home.propTypes = {}
+export default Home
